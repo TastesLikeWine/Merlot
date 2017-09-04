b5f6b6c2065d7526bbecbe60a0713ac2a44f4739
TastesLikeWine <31607051+TastesLikeWine@users.noreply.github.com>
2017-09-03 23:07:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Conditional
+
+{
+   
+   
+    class Program
+    {
+        static void Main()
+                   
+            {
+            Console.WriteLine("Please enter a number Between 1 and 10");
+            int num = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Please enter a number Between 1 and 10");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+
+
+            if (num < num2 ) 
+            {
+                Console.WriteLine("{0} is less than {1}", num, num2) ;
+             }
+            if (num == num2)
+            {
+                Console.WriteLine("{0} is the same as {1}", num, num2);
+            }
+            else 
+            {
+                Console.WriteLine("{0} is larger than {1}", num, num2);
+            }
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
